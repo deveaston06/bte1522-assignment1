f67d3601209a70fdbbd50533168f918bda9de545
Devallina <deveaston06@gmail.com>
2024-04-15 19:04:19 +0800

    Update Activity8_TG23045.py

--- Activity8_TG23045.py
+++ Activity8_TG23045.py
@@ -25,10 +25,11 @@ play_height = 30 # size of the player square
 global x, y # Global variables to store the player's position
 x = width / 2 # Starting position at the center horizontally
 y = height - 2 * play_height # Starting position near the bottom of the screen
+play_speed = 15
 
 # Step 2-2: Define a function called 'drawplayer()' to draw the player
 def drawplayer():
-    global player, x, y
+    global player, x, y, play_speed
     
     # Create a Rectangle representating the player character using  the 'pygame.Rect()' function
     # The rectange is positioned at coordinates (x, y) with a width and height of 'play_size'
@@ -38,9 +39,9 @@ def drawplayer():
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                x -= 30 # Move the player 5 pixel to the left
+                x -= play_speed # Move the player 5 pixel to the left
             elif event.key == pygame.K_RIGHT:
-                x += 30 # Move the player 5 pixel to the right
+                x += play_speed # Move the player 5 pixel to the right
                 
     # Step 3-2 Limit Player Movement within the screen boundaries
     # Ensure the player does not go outside the left edge of the screen
@@ -149,7 +150,7 @@ def timer():
     
     # Step 6-4: Display Time on the Screen
     # Create a text label showing the elapsed time
-    text = "Time: " + str(30 - int(total_time))
+    text = "Time: " + str(60 - int(total_time))
     
     # Choose a font and size for the time label
     myFont = pygame.font.SysFont("monospace", 25)
@@ -162,10 +163,86 @@ def timer():
     screen.blit(time_label, (width / 2, 0))
     
     # Step 6-5: Check for Game Over Condition
-    if total_time > 30:
+    if total_time > 60:
         return True # If more than 30 seconds have passed, return True (game over)
     else:
-        return False # If less than or equal to 30 
+        return False # If less than or equal to 30
+    
+# Step 8-1: Initialize Speed Power Up
+spd_last_reset_time = time.time() - 4
+spd_is_reset = True
+spd_is_picked_up = False
+spd_width = 30
+spd_height = 30
+spd_color = (255, 0, 255)
+y_spd = 0
+x_spd = random.randint(0, width - spd_width)
+
+def update_speed_powerup():
+    global speed_power_up, x_spd, y_spd, spd_width, spd_height, spd_is_reset, spd_last_reset_time, spd_is_picked_up, play_speed
+    
+    speed_power_up = pygame.Rect(x_spd, y_spd, spd_width, spd_height)
+    
+    if not spd_is_reset: # if speed power up is NOT reset
+        if y_spd > height: # if power up y position is more than screen height
+            spd_is_reset = True # set TRUE to speed power up is reset
+            spd_last_reset_time = time.time() # set last time speed power up reset to Now
+        else: # else move the power up downwards and draw the power up
+            y_spd += 5  
+            pygame.draw.ellipse(screen, spd_color, speed_power_up)
+    
+    if player.colliderect(speed_power_up) and not spd_is_reset: # if speed power up is NOT reset and player picked up speed power up
+        play_speed = 30 # set the player speed to 30 pixels
+        spd_is_reset = True # set TRUE to speed power up is reset
+        spd_last_reset_time = time.time() # set last time speed power up reset to Now
+        spd_is_picked_up = True
+        
+    if time.time() > spd_last_reset_time + 10 and spd_is_reset: # if speed power up is reset and elapsed more than 10 seconds since last time reset power up
+        spd_is_reset = False # set FALSE to speed power up is reset
+        y_spd = 0 # Reset the y-coordinate to the top of the screen
+        x_spd = random.randint(0, width - spd_width) # Appear at random x-coordinate
+        
+    if time.time() > spd_last_reset_time + 5 and spd_is_picked_up: # if speed power up is picked up and elapsed more than 5 seconds since last time reset power up
+        spd_is_picked_up = False # set FALSE to speed power up is picked up
+        play_speed = 15 # reset the player speed back to 15 pixels
+        
+# Step 8-3: Initialize Expand Power Up
+exp_last_reset_time = time.time()
+exp_is_reset = True
+exp_is_picked_up = False
+exp_width = 30
+exp_height = 30
+exp_color = (0, 255, 255)
+y_exp = 0
+x_exp = random.randint(0, width - exp_width)
+
+def update_expand_powerup():
+    global expand_power_up, x_exp, y_exp, exp_width, exp_height, exp_is_reset, exp_last_reset_time, exp_is_picked_up, play_width
+    
+    expand_power_up = pygame.Rect(x_exp, y_exp, exp_width, exp_height)
+    
+    if not exp_is_reset: # if expand power up is NOT reset
+        if y_exp > height: # if power up y position is more than screen height
+            exp_is_reset = True # set TRUE to expand power up is reset
+            exp_last_reset_time = time.time() # set expand power up last reset time  to Now
+        else: # else move the power up downwards and draw the power up
+            y_exp += 5
+            pygame.draw.ellipse(screen, exp_color, expand_power_up)
+    
+    if player.colliderect(expand_power_up) and not exp_is_reset: # if expand power up is NOT reset and player picked up expand power up
+        play_width = 60 # set the player width to 60 pixels
+        exp_is_reset = True # set TRUE to expand power up is reset
+        exp_last_reset_time = time.time() # set expand power up last reset time to Now
+        exp_is_picked_up = True
+        
+    if time.time() > exp_last_reset_time + 10 and exp_is_reset: # if expand power up is reset and elapsed more than 10 seconds since power up last reset time 
+        exp_is_reset = False # set FALSE to expand power up is reset
+        y_exp = 0 # Reset the y-coordinate to the top of the screen
+        x_exp = random.randint(0, width - exp_width) # Appear at random x-coordinate
+    
+    if time.time() > exp_last_reset_time + 5 and exp_is_picked_up: # if expand power up is picked up and elapsed more than 5 seconds since power up last reset time 
+        exp_is_picked_up = False # set FALSE to expand power up is picked up
+        play_width = 30 # reset the player width back to 30 pixels
 
 # Step 1-5: Starting the game loop
 # Step 6-6: Starting the game over loop
@@ -184,6 +261,12 @@ while not game_over:
     
     # Step 6-7: Call the Time Function
     game_over = timer() # Call the 'timer()'
+    
+    # Step 8-2: Call the Update Extra Time Power Up Function
+    update_speed_powerup()
+    
+    # Step 8-4: Call the Update Expand Power Up Function
+    update_expand_powerup()
     
     pygame.time.delay(50) # Add a small delay to control the speed of the game.
     
